2a83df68dbd4bbe57d3e1f2be2c6c67ce81a570b
jiangrouxian <110378108+jiangrouxian@users.noreply.github.com>
2022-08-01 17:28:10 +0800

    Jiang (#3)

--- src/UApi.php
+++ src/UApi.php
@@ -15,8 +15,14 @@ class UApi
 {
     use HasSignature;
 
+    /**
+     * @var Config
+     */
     protected Config $config;
 
+    /**
+     * @var string
+     */
     protected $baseUri = 'http://uapis.ten.sdream.top/apis/api';
 
     public function __construct(Config $config)
@@ -30,6 +36,40 @@ class UApi
             'base_uri' => $this->baseUri,
             'timeout' => 1.5,
         ]);
+    }
+
+    /**
+     * 发送短信
+     *
+     * @param  string  $mobile
+     * @param  string  $content
+     * @return array
+     * @throws HttpException
+     * @throws MissingArgumentException
+     * @throws GuzzleException
+     */
+    public function sms(array $params): array
+    {
+        $this->checkRequireParameters(['mobile', 'content'], $params);
+        try {
+            $json = $this->client()
+                ->post('sms', [
+                    RequestOptions::JSON => [
+                        'biz_content' => [
+                            'mobile' => $params['mobile'],
+                            'content' => $params['content'],
+                        ],
+                        'agent_id' => $this->config->getAgentId(),
+                        'sign' => $this->sign($params),
+                    ]
+                ])->getBody()
+                ->getContents();
+            $data = Json::decode($json);
+
+            return $data['code'] ? $data['data'] : throw new Exception($data['msg'], $data['code']);
+        } catch (Exception $e) {
+            throw new HttpException($e->getMessage(), $e->getCode(), $e);
+        }
     }
 
     /**
@@ -48,15 +88,57 @@ class UApi
         try {
             $json = $this->client()
                 ->post('idcard', [
-                    RequestOptions::JSON => [
-                        'biz_content' => [
-                            'cardno' => $params['cardno'],
-                            'name' => $params['name'],
-                        ],
-                        'agent_id' => $this->config->getAgentId(),
-                        'sign' => $this->sign($params),
+                        RequestOptions::JSON => [
+                            'biz_content' => [
+                                'cardno' => $params['cardno'],
+                                'name' => $params['name'],
+                            ],
+                            'agent_id' => $this->config->getAgentId(),
+                            'sign' => $this->sign($params),
+                        ]
                     ]
-                ])->getBody()
+                )->getBody()
+                ->getContents();
+            $data = Json::decode($json);
+
+            return $data['code'] ? $data['data'] : throw new Exception($data['msg'], $data['code']);
+        } catch (Exception $e) {
+            throw new HttpException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+
+    /**
+     * 联行号查询
+     *
+     * @param  string  $card  银行卡号
+     * @param  string  $province  省份
+     * @param  string  $city  城市名
+     * @param  string  $key  关键字(非必填)
+     * @return array
+     * @throws GuzzleException
+     * @throws HttpException
+     * @throws MissingArgumentException
+     */
+    public function bankaps(array $params): array
+    {
+        $this->checkRequireParameters(['province', 'city', 'card'], $params);
+        try {
+            $json = $this->client()
+                ->post(
+                    'bankaps',
+                    [
+                        RequestOptions::JSON => [
+                            'biz_content' => [
+                                'card' => $params['card'],
+                                'city' => $params['city'],
+                                'province' => $params['province'],
+                                'key' => $params['key'] ?? '',
+                            ],
+                            'agent_id' => $this->config->getAgentId(),
+                            'sign' => $this->sign($params),
+                        ]
+                    ]
+                )->getBody()
                 ->getContents();
             $data = Json::decode($json);
 
