89e25c93087a0fe3fef9616236ecb27e62c9eaff
jiangrouxian <2786505922@qq.com>
2022-08-02 09:18:01 +0800

    完善银行卡三要素认证

--- src/UApi.php
+++ src/UApi.php
@@ -149,6 +149,46 @@ class UApi
     }
 
     /**
+     * 银行卡三要素认证
+     *
+     * @param string $accountNo 银行卡号
+     * @param string $idCard 身份证号
+     * @param string $idCard 姓名
+     * @return array
+     * @throws GuzzleException
+     * @throws HttpException
+     * @throws MissingArgumentException
+     */
+
+    public function bank3Check(array $params)
+    {
+        $this->checkRequireParameters(['accountNo','idCard','idCard'],$params);
+        try {
+            $json=$this->client()
+                ->post(
+                    'bank3Check',
+            [
+                RequestOptions::JSON=>[
+                    'biz_content' =>[
+                        'accountNo'=>$params['accountNo'],
+                        'idCard'=>$params['idCard'],
+                        'name'=>$params['name'],
+                    ],
+                    'agent_id' => $this->config->getAgentId(),
+                    'sign' => $this->sign($params),
+                ]
+            ]
+                )->getBody()
+                ->getContents();
+            $data=Json::decode($json);
+
+            return $data['code'] ? $data['data'] : throw new Exception($data['msg'],$data['code']);
+        }catch (Exception $e){
+            throw new HttpException($e->getMessage(),$e->getCode(),$e);
+        }
+    }
+
+    /**
      * 验证参数
      *
      * @param  array  $required
